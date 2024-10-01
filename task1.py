d550371cfab3e80c777fcd2f9cd3aaf9777bf00e
badi172004/act-2 <badigaming172004@gmail.com>
2024-10-01 11:20:55 +0800

    banana

--- task1.py
+++ task1.py
@@ -1,7 +1,22 @@
-#myFavFruit = ["okok", "banana", "orange", "sa manok", "kutipot"] print("This is the list if my fav fruit:")
-for data in ["okok", "banana", "orange", "sa manok", "kutipot"]:
-    print(data)
-    if data "apple"
-    print("Happy Eating!")
-    if data == "banana":
-        break
+fruits int(input("how many fruit do you like "))
+ mgaFruits = []
+for i in range(fruits): 
+    ask = input("WHAT YOUR FAVORITE FRUIT : ") 
+    mgaFruits.append(ask)
+
+
+print(mgaFruits)
+
+for fruits in mgaFruits:
+    
+     if fruits == "banana":
+         print()
+    break
+elif fruits == "apple": 
+    print "HAPPY EATING"
+
+
+
+
+
+
