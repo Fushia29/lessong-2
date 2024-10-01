95258d94c1aad9cd64f5423af77a440cbf07a6f6
badi172004/act-2 <badigaming172004@gmail.com>
2024-10-01 11:12:49 +0800

    kutipot

--- task1.py
+++ task1.py
@@ -1,9 +1,7 @@
-    favfruit = ["apple", "mango", "banana", "paloklok"]
-    if fruit == "apple":
-
-    for data in fruits:
-        print(data)
-        if data in fruit:
-            print(data)
-            if data == "banana"
-        print "happy eating"
+#myFavFruit = ["okok", "banana", "orange", "sa manok", "kutipot"] print("This is the list if my fav fruit:")
+for data in ["okok", "banana", "orange", "sa manok", "kutipot"]:
+    print(data)
+    if data "apple"
+    print("Happy Eating!")
+    if data == "banana":
+        break
