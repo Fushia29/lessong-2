ffb3e172500a5b6aff82c0ffcb1a558eeb65944b
badi172004/act-2 <badigaming172004@gmail.com>
2024-10-01 11:01:14 +0800

    lami

--- task1.py
+++ task1.py
@@ -1,6 +1,12 @@
 favfruit = ["apple", "mango", "banana", "paloklok"]
 fruit = "paloklok"
+print("Favorite Fruits:")
 for data in favfruit:
-     print(data)
-     if data == fruit:
-            
+    print(data)
+    if data == fruit:
+        found = True
+        print(f"{fruit} is one of my favorite fruits!")
+        if not found:
+    print(f"{fruit} is not one of my favorite fruits.")
+    count = len(favfruit)
+print(f"\nI have {count} favorite fruits.")
